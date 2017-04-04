18fdaf67e5ad04be4862660ed4045dc8584a3f68
seieibob <joncole04412@gmail.com>
2017-04-04 14:57:15 -0400

    Update build structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# joncole.me
+
+This is the project structure I use to build my website. This constructs a static webpage and
+supports publishing to GitHub Pages.
+
+The actual website is built into the `/dist/` directory, which is then pushed to the master 
+branch when you run `deploy`.
+
+
+## Quick start:
+```
+npm install
+gulp preview
+```
+
+Publish to GitHub with `gulp deploy`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,42 @@
 var gulp = require("gulp");
 var deploy = require("gulp-gh-pages");
 var sass = require("gulp-sass");
+var minifyCSS = require('gulp-minify-css');
+var browserSync = require('browser-sync').create();
+
+
+// Static Server + watching scss/html files
+gulp.task("preview", ["build"], function() {
+
+    browserSync.init({
+        server: "./dist/"
+    });
+
+    gulp.watch("./sass/**/*.scss", ["sass"]);
+    gulp.watch("./dist/**/*.html").on('change', browserSync.reload);
+});
+
+// Build Sass files
+gulp.task("sass", function() {
+    gulp.src("./sass/style.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCSS()) // Optional minify step
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("copy-vendor-files", function() {
+    gulp.src(['./vendor/**/*']).pipe(gulp.dest('./dist/dist'));
+})
+
+gulp.task("build", ["sass", "copy-vendor-files"]);
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy({
-        branch: "master"
-    }))
+gulp.task('deploy', ["build"], function () {
+    return gulp.src("./dist/**/*")
+        .pipe(deploy({
+            branch: "master"
+        }))
 });
