07f4b7bae2eca50d34deae29e1a343b6385a41a2
seieibob <joncole04412@gmail.com>
2017-04-04 15:29:46 -0400

    Fix invasive rules in CSS, tidy up

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task("preview", ["build"], function() {
 gulp.task("sass", function() {
     gulp.src("./sass/style.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(minifyCSS()) // Optional minify step
+        //.pipe(minifyCSS()) // Optional minify step
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
